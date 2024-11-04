4120fa98c77e9b79479307335ca825e884a465c7
TumX31 <klimenok.sergey@gmail.com>
2024-11-04 18:04:07 +0200

    Initial commit for AppLogger project

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AppLogger;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("Hello from App using AppLogger!");
+        }
+    }
+}
